516c9fd4cecfb8e60a34be2c0ef6804447c5b61d
michalqq <kraciukmichal@gmail.com>
2019-06-02 15:58:09 +0200

    javascript

---
+++ src/main/resources/static/currency.js
@@ -0,0 +1,19 @@
+$(document).ready(function () {
+
+    alert("welcome asdasdasd");
+
+    currencyApi("usd");
+    currencyApi("eur");
+
+    function currencyApi(currency) {
+        $.ajax({
+            url: "http://localhost:8080/exchangerates/" + currency
+        }).then(function (data) {
+            alert(JSON.stringify(data));
+            $('#currency-' + currency).text(data.currency);
+            $('#exchange-rate-' + currency).text(data.rates[0].bid + 'zł');
+
+
+        });
+    }
+})
